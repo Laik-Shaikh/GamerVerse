4d1e278c8cb178d6142452f6346f45b78b66011d
LaikShaikh <laikshaikh@eng.rizvi.edu.in>
2022-02-09 18:37:19 +0530

    search page game UI Completed

--- App.js
+++ App.js
@@ -11,13 +11,14 @@ import MyGamesPage from './components/mygames/MyGamesPage';
 import GameHubPage from './components/gamehub/GameHubPage';
 import gamepage from './components/game/gamepage';
 import searchpagelocation from './components/search/searchpagelocation';
+import searchpagegame from './components/search/searchpagegame';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='SearchLocation'>
+      <Stack.Navigator initialRouteName='SearchGame'>
         <Stack.Screen name='Register' component={register} options={{ headerShown: false }} />
         <Stack.Screen name='Login' component={login} options={{ headerShown: false }} />
         <Stack.Screen name='Home' component={homepage} options={{ headerShown: false }} />
@@ -27,6 +28,7 @@ export default function App() {
         <Stack.Screen name='GameHub' component={GameHubPage} options={{ headerShown: false }} />
         <Stack.Screen name='Game' component={gamepage} options={{ headerShown: false }} />
         <Stack.Screen name='SearchLocation' component={searchpagelocation} options={{ headerShown: false }} />
+        <Stack.Screen name='SearchGame' component={searchpagegame} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ components/search/searchpagegame.js
@@ -0,0 +1,293 @@
+import React from 'react';
+import {View, StyleSheet, Dimensions, ImageBackground, Image, TouchableOpacity, Text, ScrollView, TextInput} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const windowWidth = Dimensions.get('screen').width;
+const windowHeight = Dimensions.get('screen').height;
+
+export default function searchpagegame(){
+
+    return(
+        <View style={styles.container}>
+            <LinearGradient
+                start = {{x:0, y:1}}
+                end = {{x:0, y:-1}}
+                colors={['#013C00', '#000000']}
+                style = {styles.background}
+            />
+
+            <ImageBackground source={require('./searchAssets/designspikes1.png')} 
+                style={styles.spikes1} />
+            <ImageBackground source={require('./searchAssets/designspikes2.png')} 
+                style={styles.spikes2} />
+            <Image source={require('./searchAssets/GamerVerseTitle.png')} 
+                style = {styles.GamerVerseTitle} />   
+            <ImageBackground source={require('./searchAssets/MenuBar.png')}
+                style = {styles.menuBar} />
+
+             {/* NavBar Buttons     */}
+
+             <TouchableOpacity style={styles.homebtn}  onPress={() => navigation.navigate("Home")}>
+                <Text style={styles.robototxt}>Home</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.profilebtn}  onPress={() => navigation.navigate("Profile")}>
+                <Text style={styles.robototxt}>Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.mygamesbtn}  onPress={() => navigation.navigate("MyGames")}>
+                <Text style={styles.highlighttxt}>My Games</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.gamehubbtn}  onPress={() => navigation.navigate("GameHub")}>
+                <Text style={styles.robototxt}>Game Hub</Text>
+            </TouchableOpacity>
+
+            <Image source={require('./searchAssets/searchIcon.png')} style={styles.searchIcon} />
+            <TextInput style={styles.InputStyle1} placeholder='Search for friends, games or tags'></TextInput>
+
+            <TouchableOpacity>
+                <Image source={require('./searchAssets/Rectangle 51.png')} style={styles.rectangleBox}  />
+                <View>
+                    <Image source={require('./searchAssets/ValoLogo.png')} style={styles.gameLogo} />
+                </View>
+            </TouchableOpacity>
+
+            <ImageBackground source={require('./searchAssets/HoriDivider.png')}
+                style = {styles.horiLine} />
+
+            <ScrollView style={styles.scrollContainer1}>
+                <Text style={styles.robototxt2}>Most Famous Players</Text>
+                <View style={styles.profileContainer}>
+                    <TouchableOpacity>
+                    <Image source={require('./searchAssets/Display Image.png')} style={styles.profileImage} />
+                    <View style={styles.ProfileText}>
+                        <Text style={styles.displayText}>SomeRandomPlayer</Text> 
+                    </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+
+
+            <ScrollView style={styles.scrollContainer2}>
+                <Text style={styles.robototxt2}>Most Famous Players</Text>
+                <View style={styles.profileContainer}>
+                    <TouchableOpacity>
+                    <Image source={require('./searchAssets/Display Image.png')} style={styles.profileImage} />
+                    <View style={styles.ProfileText}>
+                        <Text style={styles.displayText}>SomeRandomPlayer</Text> 
+                    </View>
+                    </TouchableOpacity>
+                </View>          
+            </ScrollView>
+
+        </View>
+    )
+
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        position: 'relative',
+        width : windowWidth,
+        height : windowHeight,
+    },
+
+    background: {
+        position: 'relative',
+        width: windowWidth,
+        height: windowHeight,
+    },
+
+    spikes1:{
+        resizeMode:'contain',
+        position:'absolute',
+        right: '0px',
+        height: 0.2*windowHeight,
+        width:0.15* windowWidth,
+    },
+
+    spikes2:{
+        position:'absolute',
+        bottom: '0px', 
+        resizeMode:'contain',
+        height: 0.2*windowHeight,
+        width:0.15* windowWidth,
+    },
+
+    GamerVerseTitle:{
+        position:"absolute",
+        left:0.35*windowWidth,
+        resizeMode:'contain',
+        height: 0.1*windowHeight,
+        width: 0.35*windowWidth,
+    },
+
+    menuBar:{
+        position:"absolute",
+        resizeMode:'contain',
+        top:0.10*windowHeight,
+        height: 0.05*windowHeight,
+        width: windowWidth,
+    },
+
+    searchIcon:{
+        position:"absolute",
+        resizeMode:'contain',
+        top:0.11*windowHeight,
+        left:0.7*windowWidth,
+        height: 0.03*windowHeight,
+        width: 0.03*windowWidth,
+    },
+
+    InputStyle1:{
+        "position": "absolute",
+        top: 107/1024*windowHeight,
+        right: 85/1440*windowWidth,
+        height: 42/1024*windowHeight,
+        width: 305/1440*windowWidth,
+        color: 'white',
+        fontSize: 17,
+        paddingLeft: 10,
+        paddingBottom: 2,
+        paddingTop: 3,    
+        borderBottomColor: "#FFFFFF",
+        borderBottomWidth: 1,
+        placeholderTextColor: "#FFFFFF",
+        backgroundColor: "#e5e5e500"
+    },
+
+    robototxt:{ 
+        "fontStyle": "normal",
+        "fontWeight": "500",
+        "fontSize": 14,
+        "color": "#FFFFFF"
+    },
+
+    robototxt2:{ 
+        "fontStyle": "normal",
+        "fontWeight": "500",
+        "fontSize": 24,
+        "color": "#FFFFFF"
+    },
+    
+    highlighttxt:{ 
+        "fontStyle": "normal",
+        "fontWeight": "500",
+        "fontSize": 15,
+        borderBottomColor: "#FFFFFF",
+        borderBottomWidth: 1,
+        "color": "#FFFFFF"
+    },
+   
+    homebtn:{
+        position:"absolute",
+        top:0.11*windowHeight,
+        left:0.05*windowWidth,
+        height: 0.03*windowHeight,
+        width: 0.03*windowWidth
+    },
+    profilebtn:{
+        position:"absolute",
+        top:0.11*windowHeight,
+        left:0.20*windowWidth,
+        height: 0.03*windowHeight,
+        width: 0.03*windowWidth
+    },
+    mygamesbtn:{
+        position:"absolute",
+        top:0.107*windowHeight,
+        left:0.35*windowWidth,
+        height: 0.03*windowHeight,
+    },
+    gamehubbtn:{
+        position:"absolute",
+        top:0.11*windowHeight,
+        left:0.50*windowWidth,
+        height: 0.03*windowHeight,
+        width: 0.05*windowWidth
+    },
+
+    searchBar:{
+        position:"absolute",
+        resizeMode:'contain',
+        top:0.10*windowHeight,
+        left:0.7*windowWidth,
+        height: 0.05*windowHeight,
+        width: 0.25*windowWidth,
+    },
+
+    rectangleBox:{
+        position: 'absolute',
+        resizeMode: 'contain',
+        width: 0.8*windowWidth,
+        height: 0.2*windowHeight,
+        top: -0.8*windowHeight,
+        left: 0.09*windowWidth
+    },
+
+    gameLogo:{
+        position: 'absolute',
+        resizeMode: 'contain',
+        width: 0.4*windowWidth,
+        height: 0.2*windowHeight,
+        top: -0.8*windowHeight,
+        left: 0.049*windowWidth
+    },
+
+    horiLine:{
+        position: 'absolute',
+        resizeMode: 'contain',
+        width: windowWidth,
+        height : 0.004 * windowHeight,
+        top : 0.43 * windowHeight  
+    },
+
+    scrollContainer1:{
+        position: 'absolute',
+        width: 0.3*windowWidth,
+        height: 0.46*windowHeight,
+        left: 0.17*windowWidth,
+        top: 0.5*windowHeight
+    },
+
+    scrollContainer2:{
+        position: 'absolute',
+        width: 0.3*windowWidth,
+        height: 0.46*windowHeight,
+        left: 0.60*windowWidth,
+        top: 0.5*windowHeight
+    },
+
+    profileImage:{
+        position: 'absolute',
+        resizeMode: 'contain',
+        width: 0.036*windowWidth,
+        height: 0.05*windowHeight,
+        marginTop: 0.03*windowHeight,
+        // marginTop: 0.04*windowHeight
+    },
+
+    profileContainer:{
+        position: 'relative',
+        // paddingTop: 0.08*windowHeight,
+        left: 0.02*windowWidth,
+        marginTop: 0.02*windowHeight
+   
+    },
+
+    ProfileText:{
+        // width: 0.03*windowWidth,
+        // height: 0.03*windowHeight,
+        paddingLeft: 0.04*windowWidth,
+        marginTop: 0.03*windowHeight,
+        // marginTop: 0.04*windowHeight
+    },
+
+    displayText:{ 
+        "fontStyle": "normal",
+        "fontWeight": "500",
+        "fontSize": 18,
+        "color": "#FFFFFF",
+    },
+
+
+});
