bb1189e6596d9aa59462b95b394cd86be1963a8f
MrQuazar <aartemsingh03@gmail.com>
2022-04-01 20:04:35 +0530

    working on intro screen

--- components/auth/intro.js
+++ components/auth/intro.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Text ,TouchableOpacity, View} from 'react-native'
+import { Text ,TouchableOpacity, View, Image,Dimensions,StyleSheet} from 'react-native'
 import fire from '../firebase';
 import 'firebase/auth';
 import { getAuth} from "firebase/auth";
+
+const windowWidth = Dimensions.get('screen').width;
+const windowHeight = Dimensions.get('screen').height;
 
 
 export default function Login({ navigation }) {
@@ -21,7 +24,20 @@ return (
                         }
                        }
                      } >
-                <Text>Press Me</Text>
+                <Image source={"https://firebasestorage.googleapis.com/v0/b/rcoegamerverse.appspot.com/o/Assets%2FIntroPage%2Ftapme.gif?alt=media&token=3adfbf78-f9e9-4dc5-baae-2b48dd8ca824"} style={styles.img2} />
                   </TouchableOpacity>
                   </View>)
 }
+
+const styles = StyleSheet.create({
+    img2:{
+    "position": "absolute",
+    marginTop: 0,
+    marginHorizontal: 0,
+    top: 200/1024*windowHeight,
+    right:400/1440*windowWidth,
+    height: 486/1024*windowHeight,
+    width: 615/1440*windowWidth,
+    resizeMode:'contain',
+  },
+});
